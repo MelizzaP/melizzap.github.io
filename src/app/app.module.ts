496f0eaa8fad4f405a63205fd64643237d7ca1b4
Melissa <melpatenator@gmail.com>
2017-10-26 16:34:30 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeadlineComponent } from './headline/headline.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadlineComponent
+    HeadlineComponent,
+    SummaryComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
